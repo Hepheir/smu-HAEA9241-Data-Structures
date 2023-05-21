6d5b2441c2f27d68e09b8bef2c6f1c8ea621e3f3
Hepheir <hepheir@gmail.com>
2023-05-21 19:46:05 +0900

    feat(실습 05): add skeleton code

---
+++ 실습 05 (트리)/solution.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+
+typedef class node *nptr;
+
+class node {
+    int val;
+    nptr lchild, rchild;
+
+public:
+    void init();
+    int insert(int cval, int lval, int rval);
+    void inorder_print();
+    int height();
+    int width();
+    int serach(int sval);
+    int valid(int cval, int lval, int rval);
+    int is_leaf_node(int cval);
+};
+
+int main(void) {
+
+    return 0;
+}
